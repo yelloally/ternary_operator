f8b051e1f6a313ed9c6b9591881603c689b0e1e3
Alona <122108963+yelloally@users.noreply.github.com>
2023-01-06 19:12:53 +0100

    Add files via upload

---
+++ ternary_operator/ternaryOpetaor.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //create a bool for our operator
+        bool kodParol;
+
+        //create two variables
+        // 1 - with "true" word
+        string storedPassword = "swordfish";
+
+        // 2 - with a word from person
+        string entPassword = Console.ReadLine();
+
+        //condition, where we use ternary operator 
+        kodParol = entPassword == storedPassword ? true : false;
+
+        //bool output
+        Console.WriteLine(kodParol);
+        
+    
+        Console.ReadLine();
+    }
+}
